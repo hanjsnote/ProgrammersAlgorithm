eb12d59f7bffd0a72ffd84eed59d727490f089de
hanjsnote <enlun@naver.com>
2025-07-28 18:19:38 +0900

    프로그래머스 알고리즘 잔디용

--- src/Programmers/Lv0/PCCE_no7_bus.java
+++ src/Programmers/Lv0/PCCE_no7_bus.java
@@ -2,7 +2,7 @@ package Programmers.Lv0;
 
 public class PCCE_no7_bus {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) { 
 
         int seat = 30;
         String[][] passengers = new String[20][20];
